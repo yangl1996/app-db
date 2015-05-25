7559e578fb2363717a37b9443b93d8dc530855d4
Lei Yang <yltt1234512@gmail.com>
2015-05-25 20:02:51 +0800

    test server
    Implemented a test-oriented http server, no actual usage

--- server.py
+++ server.py
@@ -1,3 +1,31 @@
 __author__ = 'yangl1996'
 import sqlite3
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+PORT_NUMBER = 8080
+
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    # Handler for the GET requests
+    def do_GET(self):
+        print('Get request received')
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World !")
+        return
+
+try:
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', PORT_NUMBER), MyHandler)
+    print('Started server on port ', PORT_NUMBER)
+
+    # Wait forever for incoming http requests
+    server.serve_forever()
+
+finally:
+    print("Error encountered, shutting down")
