a6b409c4d40e5310137959480e9739fa84fb4cce
Lei Yang <yltt1234512@gmail.com>
2015-05-25 21:34:36 +0800

    database loading

--- server.py
+++ server.py
@@ -1,15 +1,39 @@
 __author__ = 'yangl1996'
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import json
 
 hostName = "128.199.244.33"
 hostPort = 8080
+db_file_path = "database.json"
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        user_id = self.path
-        print(user_id)
+        global db_file_path
+
+        # parsing HTTP GET request
+        user_id = self.path[1:]
+        print("receiving request from ", self.client_address, " requesting user ", user_id)
+
+        # looking up from database
+        # data structure:{user_id_1: [url_1, url_2, url_3...], user_id_2: [url_1, url_2, url_3...]}
+
+        # loading database
+        database_file = open(db_file_path, 'r')
+        database = database_file.read()
+        user_table = json.loads(database)
+        database_file.close()
+
+        # database loaded as user_table
+        # searching for URLs
+        if user_id in user_table:
+            URLs = user_table[user_id]
+            # TODO: add XML generation
+
+        else:
+            # TODO: add error handling
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
