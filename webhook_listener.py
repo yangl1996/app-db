acc2156680115fadc472ce17cd59cb30e7aa6b82
Lei Yang <yltt1234512@gmail.com>
2015-06-15 22:38:46 +0800

    handle for new requests

--- webhook_listener.py
+++ webhook_listener.py
@@ -9,6 +9,7 @@ listenPort = 9999
 db_file_path = "database.json"
 appid = ""
 secret = ""
+last_image_id = 1
 
 
 class MyServer(BaseHTTPRequestHandler):
@@ -26,70 +27,22 @@ class MyServer(BaseHTTPRequestHandler):
             database = database_file.read()
             db = json.loads(database)
             database_file.close()
-            if user_id not in db['user']:
-                db['user'][user_id] = {}
-                db['user'][user_id]["image_count"] = 0
-                db['user'][user_id]["voice_count"] = 0
-                db['user'][user_id]["image"] = []
-                db['user'][user_id]["voice"] = []
-            db['user'][user_id]['image_count'] += 1
-            local_id = db['user'][user_id]['image_count']
-            local_filename = str(local_id) + ".jpg"
-            local_fileURL = "http://" + listenAddr + ":8090" + "/FS/" + user_id + "/" + local_filename
-            db['user'][user_id]['image'].append(local_fileURL)
+            db['count'] += 1
+            global last_image_id
+            last_image_id = db['count']
+            local_filename = str(last_image_id) + ".jpg"
+            local_fileURL = "http://" + listenAddr + ":8090" + "/FS/" + local_filename
+            image_metadata = {'id': last_image_id, 'name': user_id, 'url': local_fileURL, 'text': "", 'audio': ""}
+            db['user'].append(image_metadata)
             command = """
             cd FS
-            mkdir """ + user_id + """
-            cd """ + user_id + """
-            wget -O """ + local_filename + " " + image_link + """
-            """
+            wget -O {} {}""".format(local_filename, image_link)
             os.system(command)
             to_write = json.dumps(db)
             writer = open('database.json', 'w')
             writer.write(to_write)
             writer.close()
 
-        elif data['MediaType'] == 'voice':
-            user_id = data['Usr_Id']
-            media_id = data['Media_Id']
-            print("New voice from %s, Media ID is %s." % (user_id, media_id))
-            database_file = open(db_file_path, 'r')
-            database = database_file.read()
-            db = json.loads(database)
-            database_file.close()
-            if user_id not in db['user']:
-                db['user'][user_id] = {}
-                db['user'][user_id]["image_count"] = 0
-                db['user'][user_id]["voice_count"] = 0
-                db['user'][user_id]["image"] = []
-                db['user'][user_id]["voice"] = []
-
-            payload = {'grant_type': 'client_credential', 'appid': appid, 'secret': secret}
-            r = requests.get("https://api.weixin.qq.com/cgi-bin/token", params=payload)
-            return_token = json.loads(r.text)
-            token = return_token['access_token']
-
-            # form the URL
-            payload = {'access_token': token, 'media_id': media_id}
-            q = requests.get("https://api.weixin.qq.com/cgi-bin/media/get", params=payload)
-            remote_url = q.url
-
-            db['user'][user_id]['voice_count'] += 1
-            local_id = db['user'][user_id]['voice_count']
-            local_filename = str(local_id) + ".voice"
-            local_fileURL = "http://" + listenAddr + ":8090" + "/FS/" + user_id + "/" + local_filename
-            db['user'][user_id]['voice'].append(local_fileURL)
-            command = """
-            cd FS
-            mkdir """ + user_id + """
-            cd """ + user_id + """
-            wget -O """ + local_filename + " " + remote_url + """
-            """
-            os.system(command)
-            to_write = json.dumps(db)
-            writer = open('database.json', 'w')
-            writer.write(to_write)
-            writer.close()
         else:
             print(post_body)
 
